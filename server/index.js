038177ff484385317711b81f0bbb905ff4f96b68
Anuj Gangwar <agangwar7819@gmail.com>
2026-06-23 01:48:35 +0530

    Update CORS origin for the application

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin:"https://interviewiq-52sz.onrender.com",
+    origin:"https://preppilot-5lwd.onrender.com",
     credentials:true
 }))
 
