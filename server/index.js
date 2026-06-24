e39d1d85fa6310677161a3945b909ee60cd4fe0c
AnujGangwar01 <agangwar7819@gmail.com>
2026-06-24 19:16:40 +0530

    updated backend server to include new

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin:"https://interviewiq-52sz.onrender.com",
+    origin:"https://preppilot-backened.onrender.com",
     credentials:true
 }))
 
